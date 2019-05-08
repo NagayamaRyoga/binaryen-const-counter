7ba4d74b1ac1c51a56c3667e2e29546a01dd3f5b
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-05-08 09:36:35 +0900

    Create the pass object

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 
 #include <wasm-io.h>
 
+// Counting Pass Class
+class CountingPass : public wasm::Pass {
+public:
+    // On module object callback
+    void run([[maybe_unused]] wasm::PassRunner *pass_runner, wasm::Module *module) override {
+        int count = 0;
+
+        (void)module;
+
+        std::cout << count << " const instruction(s)" << std::endl;
+    }
+
+    bool modifiesBinaryenIR() override {
+        return false;
+    }
+};
+
 int main(int argc, char *argv[]) {
     try {
         if (argc != 2) {
@@ -13,6 +30,11 @@ int main(int argc, char *argv[]) {
         // Read WebAssembly Module
         wasm::Module module;
         wasm::ModuleReader{}.read(argv[1], module);
+
+        // Register passes to a pass runner
+        wasm::PassRunner pass_runner{&module};
+        pass_runner.add<CountingPass>();
+        pass_runner.run();
     } catch (const wasm::ParseException &e) {
         e.dump(std::cerr);
 
